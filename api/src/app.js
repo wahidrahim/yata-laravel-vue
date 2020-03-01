0439d44794e305e8660742e55b568c996c2c6665
Wahid Rahim <wahidrahim@gmail.com>
2020-03-01 02:41:08 -0500

    dev script

--- api/src/app.js
+++ api/src/app.js
@@ -11,8 +11,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
-app.get('/', (req, res) => {
-  res.send('hello world')
+app.get('/ping', (req, res) => {
+  res.send('pong!')
 })
 
 export default app
