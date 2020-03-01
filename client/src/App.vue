563f5cfebb896d7d84c5b679695b9219aa1f85a2
Wahid Rahim <wahidrahim@gmail.com>
2020-02-29 19:05:27 -0500

    todo input form

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,32 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="todo-container m-3">
+      <b-card>
+        <b-form>
+          <b-form-input type="text" />
+          <b-button class="mt-2" type="submit" variant="primary" block>
+            Add
+          </b-button>
+        </b-form>
+      </b-card>
     </div>
-    <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+<style lang="scss" scoped>
+.todo-container {
+  max-width: 320px;
 }
 </style>
