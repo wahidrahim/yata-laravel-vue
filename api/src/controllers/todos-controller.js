b304cf7415ffc202076fc64f784c987f30ade74d
Wahid Rahim <wahidrahim@gmail.com>
2020-03-01 03:09:16 -0500

    api controller update

--- api/src/controllers/todos-controller.js
+++ api/src/controllers/todos-controller.js
@@ -1,7 +1,20 @@
-export const index = (req, res, next) => {
-  res.json([
-    {
-      task: 'test'
-    }
-  ])
+export const index = (req, res) => {
+  res.json({
+    message: 'show all todos'
+  })
+}
+
+export const create = (req, res) => {
+  res.json({
+    message: 'create new todo',
+    body: req.body
+  })
+}
+
+export const update = (req, res) => {
+  res.send(req.params)
+}
+
+export const destroy = (req, res) => {
+  res.send(req.params)
 }
--- api/src/routes/todos.js
+++ api/src/routes/todos.js
@@ -1,8 +1,11 @@
 import express from 'express'
-import { index } from '../controllers/todos-controller'
+import { index, create, update, destroy } from '../controllers/todos-controller'
 
 const router = express.Router()
 
 router.get('/', index)
+router.post('/', create)
+router.put('/:id', update)
+router.delete('/:id', destroy)
 
 export default router
